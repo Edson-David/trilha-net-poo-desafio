45c5089a0c04cd154d6a3ba66b8164314b1a6574
Edson-David <edsondavid22@outlook.com>
2024-11-08 11:52:48 -0300

    Desafio completo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,20 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+
+        protected int Memoria;
+        protected string IMEI;
+        protected string Modelo;
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // DONE: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
@@ -21,6 +28,6 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void InstalarAplicativo(string aplicativo);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iPhone = new Iphone(numero: "123", modelo: "7", imei: "1234", memoria: 256 );
+Smartphone nokia = new Nokia( numero: "1235", modelo: "9", imei: "4231", memoria: 64);
+
+Console.WriteLine("Smarphone iPhone");
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Genshin");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
